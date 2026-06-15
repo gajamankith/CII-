b1a593e107372f7c30e1c5fddd5ceaff6d8ef157
gajamankith <asustuf5370@gmail.com>
2026-06-15 06:03:06 +0000

    hi

---
+++ notifications.py
@@ -0,0 +1,14 @@
+Your package has arrived.
+
+Package ID: PKG12345
+OTP: 458921
+
+Collect from Campus Courier Hub.
+
+
+Package Received 📦
+
+Package ID: PKG12345
+OTP: 458921
+
+Pickup after classes.
